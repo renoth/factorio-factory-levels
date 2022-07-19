258d282bce3183819bc8121ee3da001d25ac9feb
Renoth, Johannes (StMELF-Extern) <Johannes.Renoth@stmelf.bayern.de>
2022-07-19 23:02:57 +0200

    0.0.1 - fix bug

--- factory-levels/control.lua
+++ factory-levels/control.lua
@@ -6,22 +6,22 @@ script.on_load(function()
 
 end)
 
-
 requireditems = {
 }
 
 for i = 1, 100, 1 do
-    table.insert(requireditems, 10 + i * 25)
+    table.insert(requireditems, i * 25)
 end
 
 function upgrade_factory(surface, targetname, sourceentity)
-    log(serpent.block(entity))
-    local createdentity = surface.create_entity { name = targetname,
-                            source = sourceentity,
-                            fast_replace = true,
-                            position = sourceentity.position,
-                            force = "player" }
-    createdentity.products_finished = sourceentity.products_finished
+    log(serpent.block(sourceentity))
+    local created = surface.create_entity { name = targetname,
+                                            source = sourceentity,
+                                            fast_replace = true,
+                                            create_build_effect_smoke = false,
+                                            position = sourceentity.position,
+                                            force = "player" }
+    log(created)
     sourceentity.destroy()
 end
 
@@ -31,7 +31,6 @@ function replace_assembler(entities, surface)
             upgrade_factory(surface, "assembling-machine-level-1", entity)
         else
             local currentlevel = tonumber(string.match(entity.name, "%d+$"))
-            log(currentlevel)
             if (entity.products_finished > requireditems[currentlevel] and currentlevel < 100) then
                 upgrade_factory(surface, "assembling-machine-level-" .. (currentlevel + 1), entity)
             end
