3f87452a139eb2b2045fefd2ad052816bba20fce
Renoth, Johannes (StMELF-Extern) <Johannes.Renoth@stmelf.bayern.de>
2022-07-20 21:05:11 +0200

    0.0.3 - assembler 2 and 3 with levels - stone furnaces with 25 levels - steel furnaces with 100 levels

--- factory-levels/control.lua
+++ factory-levels/control.lua
@@ -13,8 +13,22 @@ end
 requireditems_assembler1 = {
 }
 
+requireditems_assembler2 = {
+}
+
+requireditems_assembler3 = {
+}
+
 for i = 1, 25, 1 do
     table.insert(requireditems_assembler1, math.floor(10 + math.pow(i, 2.86136))) -- level 25 is 10k items
+end
+
+for i = 1, 50, 1 do
+    table.insert(requireditems_assembler2, math.floor(10 + math.pow(i, 3))) -- level 50 is 125k items
+end
+
+for i = 1, 100, 1 do
+    table.insert(requireditems_assembler3, math.floor(10 + math.pow(i, 3))) -- level 100 is 1M items
 end
 
 function upgrade_factory(surface, targetname, sourceentity)
@@ -33,26 +47,73 @@ function upgrade_factory(surface, targetname, sourceentity)
     for _, item in pairs(old_on_ground) do
         item.destroy()
     end
+
+    return created
 end
 
 function replace_assembler(entities, surface)
     for _, entity in pairs(entities) do
         if (entity.name == "assembling-machine-1" and entity.products_finished > 0) then
             upgrade_factory(surface, "assembling-machine-1-level-1", entity)
+        elseif (entity.name == "assembling-machine-2" and entity.products_finished > 0) then
+            upgrade_factory(surface, "assembling-machine-2-level-1", entity)
+        elseif (entity.name == "assembling-machine-3" and entity.products_finished > 0) then
+            upgrade_factory(surface, "assembling-machine-3-level-1", entity)
         elseif string_starts_with(entity.name, "assembling-machine-1-level-") then
             local currentlevel = tonumber(string.match(entity.name, "%d+$"))
             if (entity.products_finished > requireditems_assembler1[currentlevel] and currentlevel < 25) then
                 upgrade_factory(surface, "assembling-machine-1-level-" .. (currentlevel + 1), entity)
             elseif (currentlevel == 25 and entity.name == "assembling-machine-1-level-25") then
-                upgrade_factory(surface, "assembling-machine-2", entity)
+                local created = upgrade_factory(surface, "assembling-machine-2", entity)
+                created.products_finished = 0
+            end
+        elseif string_starts_with(entity.name, "assembling-machine-2-level-") then
+            local currentlevel = tonumber(string.match(entity.name, "%d+$"))
+            if (entity.products_finished > requireditems_assembler2[currentlevel] and currentlevel < 50) then
+                upgrade_factory(surface, "assembling-machine-2-level-" .. (currentlevel + 1), entity)
+            elseif (currentlevel == 50 and entity.name == "assembling-machine-2-level-50") then
+                local created = upgrade_factory(surface, "assembling-machine-3", entity)
+                created.products_finished = 0
+            end
+        elseif string_starts_with(entity.name, "assembling-machine-3-level-") then
+            local currentlevel = tonumber(string.match(entity.name, "%d+$"))
+            if (entity.products_finished > requireditems_assembler3[currentlevel] and currentlevel < 100) then
+                upgrade_factory(surface, "assembling-machine-3-level-" .. (currentlevel + 1), entity)
             end
         end
     end
 end
 
-script.on_nth_tick(200, function(event)
+function replace_smelters(entities, surface)
+    for _, entity in pairs(entities) do
+        local currentlevel = tonumber(string.match(entity.name, "%d+$"))
+
+        if (entity.name == "stone-furnace" and entity.products_finished > 0) then
+            upgrade_factory(surface, "stone-furnace-level-1", entity)
+
+        elseif (entity.name == "steel-furnace" and entity.products_finished > 0) then
+            upgrade_factory(surface, "steel-furnace-level-1", entity)
+        elseif string_starts_with(entity.name, "stone-furnace-level-") then
+            if (entity.products_finished > requireditems_assembler1[currentlevel] and currentlevel < 25) then
+                upgrade_factory(surface, "stone-furnace-level-" .. (currentlevel + 1), entity)
+            elseif (currentlevel == 25 and entity.name == "stone-furnace-level-25") then
+                local created = upgrade_factory_(surface, "steel-furnace", entity)
+                created.products_finished = 0
+            end
+        elseif string_starts_with(entity.name, "steel-furnace-level-") then
+            if (entity.products_finished > requireditems_assembler2[currentlevel] and currentlevel < 100) then
+                upgrade_factory(surface, "steel-furnace-level-" .. (currentlevel + 1), entity)
+            end
+        end
+    end
+end
+
+script.on_nth_tick(300, function(event)
     for _, surface in pairs(game.surfaces) do
         local assemblers = surface.find_entities_filtered { type = "assembling-machine" }
         replace_assembler(assemblers, surface)
+
+        local smelters = surface.find_entities_filtered { type = "furnace" }
+        replace_smelters(smelters, surface)
     end
 end)
--- factory-levels/prototypes/entity/entity.lua
+++ factory-levels/prototypes/entity/entity.lua
@@ -1,24 +1,96 @@
 for i = 1, 25, 1 do
-    local leveltint = { r = 0.1 + 0.032 * i, g = 0.1 + 0.032 * i, b = 0.1 + 0.032 * i, 1 }
+    local leveltint = { r = 0.1 + 0.01 * i, g = 0.1 + 0.01 * i, b = 0.1 + 0.032 * i, 1 }
 
     local assemblyLeveled = util.table.deepcopy(data.raw["assembling-machine"]["assembling-machine-1"])
 
     assemblyLeveled.name = "assembling-machine-1-level-" .. i
-    assemblyLeveled.icons = { { icon = "__base__/graphics/icons/assembling-machine-1.png", tint = { r = 0.032 * i, g = 0.032 * i, b = 0.032 * i, 1 } } }
+    assemblyLeveled.icons = { { icon = "__base__/graphics/icons/assembling-machine-1.png", tint = { r = 0.01 * i, g = 0.01 * i, b = 0.032 * i, 1 } } }
     assemblyLeveled.minable.result = "assembling-machine-1"
     assemblyLeveled.animation.layers[1].hr_version.tint = leveltint
     assemblyLeveled.animation.layers[1].tint = leveltint
     assemblyLeveled.animation.layers[2].hr_version.tint = leveltint
     assemblyLeveled.animation.layers[2].tint = leveltint
     assemblyLeveled.energy_usage = (75 + 2.5 * i) .. "kW"
-    assemblyLeveled.crafting_speed = 0.5 + i * 0.025
-    assemblyLeveled.requireditems = i * i + 10
-    assemblyLeveled.maxhealth = 200 + 5 * i
-    assemblyLeveled.energy_source.emissions_per_minute = 4 + 0.15 * i
-    if (i == 25) then
-        assemblyLeveled.nextlevel = "assembling-machine-2"
-    end
-    assemblyLeveled.nextlevel = "assembling-machine-1-level-" .. (i + 1)
+    assemblyLeveled.crafting_speed = 0.5 + i * 0.01
+    assemblyLeveled.energy_source.emissions_per_minute = 4 + 0.04 * i
 
     data:extend({ assemblyLeveled })
+end
+
+for i = 1, 50, 1 do
+    local leveltint = { r = 0.1 + 0.010 * i, g = 0.1 + 0.010 * i, b = 1 - 0.016 * i, 1 }
+
+    local assemblyLeveled = util.table.deepcopy(data.raw["assembling-machine"]["assembling-machine-2"])
+
+    assemblyLeveled.name = "assembling-machine-2-level-" .. i
+    assemblyLeveled.icons = { { icon = "__base__/graphics/icons/assembling-machine-2.png", tint = { r = 0.1 + 0.010 * i, g = 0.1 + 0.010 * i, b = 0.1 + 0.016 * i, 1 } } }
+    assemblyLeveled.minable.result = "assembling-machine-2"
+    assemblyLeveled.animation.layers[1].hr_version.tint = leveltint
+    assemblyLeveled.animation.layers[1].tint = leveltint
+    assemblyLeveled.animation.layers[2].hr_version.tint = leveltint
+    assemblyLeveled.animation.layers[2].tint = leveltint
+    assemblyLeveled.energy_usage = (150 + 4.5 * i) .. "kW"
+    assemblyLeveled.crafting_speed = 0.75 + i * 0.01
+    assemblyLeveled.maxhealth = 350 + i
+    assemblyLeveled.energy_source.emissions_per_minute = 3 + 0.04 * i
+
+    data:extend({ assemblyLeveled })
+end
+
+for i = 1, 100, 1 do
+    local leveltint = { r = 0.1 + 0.008 * i, g = 0.1 + 0.002 * i, b = 0.1 + 0.004 * i, 1 }
+
+    local assemblyLeveled = util.table.deepcopy(data.raw["assembling-machine"]["assembling-machine-3"])
+
+    assemblyLeveled.name = "assembling-machine-3-level-" .. i
+    assemblyLeveled.icons = { { icon = "__base__/graphics/icons/assembling-machine-3.png", tint = { r = 0.1 + 0.008 * i, g = 0.1 + 0.002 * i, b = 0.1 + 0.004 * i, 1 } } }
+    assemblyLeveled.minable.result = "assembling-machine-3"
+    assemblyLeveled.animation.layers[1].hr_version.tint = leveltint
+    assemblyLeveled.animation.layers[1].tint = leveltint
+    assemblyLeveled.animation.layers[2].hr_version.tint = leveltint
+    assemblyLeveled.animation.layers[2].tint = leveltint
+    assemblyLeveled.energy_usage = (375 + 7.5 * i) .. "kW"
+    assemblyLeveled.crafting_speed = 1.25 + i * 0.0175
+    assemblyLeveled.maxhealth = 400 + i * 4
+    assemblyLeveled.energy_source.emissions_per_minute = 2 + 0.04 * i
+
+    data:extend({ assemblyLeveled })
+end
+
+-- Furnaces
+
+for i = 1, 25, 1 do
+    local leveltint = { r = 0.1 + 0.032 * i, g = 0.1 + 0.032 * i, b = 0.1 + 0.01 * i, 1 }
+
+    local furnaceLeveled = util.table.deepcopy(data.raw["furnace"]["stone-furnace"])
+
+    furnaceLeveled.name = "stone-furnace-level-" .. i
+    furnaceLeveled.minable.result = "stone-furnace"
+    furnaceLeveled.animation.layers[1].hr_version.tint = leveltint
+    furnaceLeveled.animation.layers[1].tint = leveltint
+    furnaceLeveled.animation.layers[2].hr_version.tint = leveltint
+    furnaceLeveled.animation.layers[2].tint = leveltint
+    furnaceLeveled.energy_usage = (90 - 0.25 * i) .. "kW"
+    furnaceLeveled.crafting_speed = 1 + i * 0.04
+    furnaceLeveled.energy_source.emissions_per_minute = 2 + 0.04 * i
+
+    data:extend({ furnaceLeveled })
+end
+
+for i = 1, 100, 1 do
+    local leveltint = { r = 0.1 + 0.008 * i, g = 0.1 + 0.002 * i, b = 0.1 + 0.008 * i, 1 }
+
+    local furnaceLeveled = util.table.deepcopy(data.raw["furnace"]["steel-furnace"])
+
+    furnaceLeveled.name = "steel-furnace-level-" .. i
+    furnaceLeveled.minable.result = "steel-furnace"
+    furnaceLeveled.animation.layers[1].hr_version.tint = leveltint
+    furnaceLeveled.animation.layers[1].tint = leveltint
+    furnaceLeveled.animation.layers[2].hr_version.tint = leveltint
+    furnaceLeveled.animation.layers[2].tint = leveltint
+    furnaceLeveled.energy_usage = (90 - 0.3 * i) .. "kW"
+    furnaceLeveled.crafting_speed = 2 + i * 0.06
+    furnaceLeveled.energy_source.emissions_per_minute = 4 + 0.12 * i
+
+    data:extend({ furnaceLeveled })
 end
