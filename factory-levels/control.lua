02b61beebc9994c55d441829e84970774e1f4c3d
Renoth, Johannes (StMELF-Extern) <Johannes.Renoth@stmelf.bayern.de>
2022-07-25 08:50:48 +0200

    0.2.5 - refactoring from caitsith2 - set spill to true to avoid spilling contents of upgraded assemblers #8

--- factory-levels/control.lua
+++ factory-levels/control.lua
@@ -1,13 +1,13 @@
 script.on_init(function()
-    global.stored_products_finished_assemblers = { 1000000, 55555 }
-    global.stored_products_finished_furnaces = { 1000000, 55555 }
+	global.stored_products_finished_assemblers = { 1000000, 55555 }
+	global.stored_products_finished_furnaces = { 1000000, 55555 }
 end)
 
 script.on_load(function()
 end)
 
 function string_starts_with(str, start)
-    return str:sub(1, #start) == start
+	return str:sub(1, #start) == start
 end
 
 machines = {
@@ -29,7 +29,7 @@ machines = {
 		level_name = "assembling-machine-3-level-",
 		max_level = 100
 	},
-	
+
 	-- Smelters
 	{
 		name = "stone-furnace",
@@ -50,44 +50,45 @@ required_items_for_levels = {
 exponent = settings.startup["factory-levels-exponent"].value
 
 for i = 1, 100, 1 do
-    table.insert(required_items_for_levels, math.floor(1 + math.pow(i, exponent)))
+	table.insert(required_items_for_levels, math.floor(1 + math.pow(i, exponent)))
 end
 
 function determine_level(finished_products_count)
-    local should_have_level = 1
+	local should_have_level = 1
 
-    for level, min_count_required_for_level in pairs(required_items_for_levels) do
-        if finished_products_count >= min_count_required_for_level then
-            should_have_level = level
-        end
-    end
+	for level, min_count_required_for_level in pairs(required_items_for_levels) do
+		if finished_products_count >= min_count_required_for_level then
+			should_have_level = level
+		end
+	end
 
-    return should_have_level
+	return should_have_level
 end
 
 function upgrade_factory(surface, targetname, sourceentity)
-    local count = sourceentity.products_finished
-    local box = sourceentity.bounding_box
-    local created = surface.create_entity { name = targetname,
-                                            source = sourceentity,
-                                            fast_replace = true,
-                                            create_build_effect_smoke = false,
-                                            position = sourceentity.position,
-                                            force = "player" }
-    created.products_finished = count;
-    sourceentity.destroy()
+	local count = sourceentity.products_finished
+	local box = sourceentity.bounding_box
+	local created = surface.create_entity { name = targetname,
+											source = sourceentity,
+											fast_replace = true,
+											spill = false,
+											create_build_effect_smoke = false,
+											position = sourceentity.position,
+											force = "player" }
+	created.products_finished = count;
+	sourceentity.destroy()
 
-    local old_on_ground = surface.find_entities_filtered { area = box, name = 'item-on-ground' }
-    for _, item in pairs(old_on_ground) do
-        item.destroy()
-    end
+	local old_on_ground = surface.find_entities_filtered { area = box, name = 'item-on-ground' }
+	for _, item in pairs(old_on_ground) do
+		item.destroy()
+	end
 
-    return created
+	return created
 end
 
 function replace_machines(entities, surface)
-    for _, entity in pairs(entities) do
-        local should_have_level = determine_level(entity.products_finished)
+	for _, entity in pairs(entities) do
+		local should_have_level = determine_level(entity.products_finished)
 		for _, machine in pairs(machines) do
 			if (entity.name == machine.name and entity.products_finished > 0) then
 				upgrade_factory(surface, machine.level_name .. math.min(should_have_level, machine.max_level), entity)
@@ -104,44 +105,44 @@ function replace_machines(entities, surface)
 				end
 			end
 		end
-    end
+	end
 end
 
 script.on_nth_tick(300, function(event)
-    for _, surface in pairs(game.surfaces) do
-        local assemblers = surface.find_entities_filtered { type = {"assembling-machine", "furnace"} }
-        replace_machines(assemblers, surface)
-    end
+	for _, surface in pairs(game.surfaces) do
+		local assemblers = surface.find_entities_filtered { type = { "assembling-machine", "furnace" } }
+		replace_machines(assemblers, surface)
+	end
 end)
 
 script.on_event(
-        defines.events.on_player_mined_entity,
-        function(event)
-            if (event.entity ~= nil and event.entity.products_finished ~= nil and event.entity.products_finished > 0) then
-                if event.entity.type == "furnace" then
-                    table.insert(global.stored_products_finished_furnaces, event.entity.products_finished)
-                    table.sort(global.stored_products_finished_furnaces)
-                end
+		defines.events.on_player_mined_entity,
+		function(event)
+			if (event.entity ~= nil and event.entity.products_finished ~= nil and event.entity.products_finished > 0) then
+				if event.entity.type == "furnace" then
+					table.insert(global.stored_products_finished_furnaces, event.entity.products_finished)
+					table.sort(global.stored_products_finished_furnaces)
+				end
 
-                if event.entity.type == "assembling-machine" then
-                    table.insert(global.stored_products_finished_assemblers, event.entity.products_finished)
-                    table.sort(global.stored_products_finished_assemblers)
-                end
-            end
-        end,
-        { { filter = "type", type = "assembling-machine" },
-          { filter = "type", type = "furnace" } })
+				if event.entity.type == "assembling-machine" then
+					table.insert(global.stored_products_finished_assemblers, event.entity.products_finished)
+					table.sort(global.stored_products_finished_assemblers)
+				end
+			end
+		end,
+		{ { filter = "type", type = "assembling-machine" },
+		  { filter = "type", type = "furnace" } })
 
 script.on_event(
-        defines.events.on_built_entity,
-        function(event)
-            if (event.created_entity ~= nil and event.created_entity.type == "assembling-machine") then
-                local finished_product_count = table.remove(global.stored_products_finished_assemblers)
-                if finished_product_count ~= nil then
-                    local should_have_level = determine_level(finished_product_count)
-                    event.created_entity.products_finished = finished_product_count
+		defines.events.on_built_entity,
+		function(event)
+			if (event.created_entity ~= nil and event.created_entity.type == "assembling-machine") then
+				local finished_product_count = table.remove(global.stored_products_finished_assemblers)
+				if finished_product_count ~= nil then
+					local should_have_level = determine_level(finished_product_count)
+					event.created_entity.products_finished = finished_product_count
 
-                    local created_entity_name = event.created_entity.name
+					local created_entity_name = event.created_entity.name
 					for _, machine in pairs(machines) do
 						if created_entity_name == machine.name then
 							local created = upgrade_factory(event.created_entity.surface, machine.level_name .. math.min(should_have_level, machine.max_level), event.created_entity)
@@ -149,27 +150,27 @@ script.on_event(
 							return
 						end
 					end
-                    return
-                end
-            end
+					return
+				end
+			end
 
-            if (event.created_entity ~= nil and event.created_entity.type == "furnace") then
-                local finished_product_count = table.remove(global.stored_products_finished_furnaces)
-                if finished_product_count ~= nil then
-                    local should_have_level = determine_level(finished_product_count)
-                    event.created_entity.products_finished = finished_product_count
+			if (event.created_entity ~= nil and event.created_entity.type == "furnace") then
+				local finished_product_count = table.remove(global.stored_products_finished_furnaces)
+				if finished_product_count ~= nil then
+					local should_have_level = determine_level(finished_product_count)
+					event.created_entity.products_finished = finished_product_count
 
-                    local created_entity_name = event.created_entity.name
-                    for _, machine in pairs(machines) do
+					local created_entity_name = event.created_entity.name
+					for _, machine in pairs(machines) do
 						if created_entity_name == machine.name then
 							local created = upgrade_factory(event.created_entity.surface, machine.level_name .. math.min(should_have_level, machine.max_level), event.created_entity)
 							created.products_finished = finished_product_count
 							return
 						end
 					end
-                    return
-                end
-            end
-        end,
-        { { filter = "type", type = "assembling-machine" },
-          { filter = "type", type = "furnace" } })
+					return
+				end
+			end
+		end,
+		{ { filter = "type", type = "assembling-machine" },
+		  { filter = "type", type = "furnace" } })
